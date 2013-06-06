b395394205d41e8bd092311af8151ee25e89f157
Barry Winata <barry.winata@yahoo.com>
2013-06-07 01:02:44 +1000

    tested ajax on launch page

--- php/application/controllers/launch.php
+++ php/application/controllers/launch.php
@@ -1,13 +1,13 @@
 <?php
 
-class Launch extends CI_Contoller 
+class Launch extends CI_Controller 
 {
 	/* Event Handler */
 	public function gauge_interest ()
 	{
-		$this->load->model('Launch_Interest', '', TRUE);
-		$sub_array('Hello');
-		$test_array = array('data' => TRUE, 'string' => $sub_array);
+		//$this->load->model('Launch_Interest', '', TRUE);
+		$sub_array['test'] = 'Hello';
+		$test_array = array('result' => TRUE, 'string' => $sub_array);
 		echo json_encode($test_array);
 	}
 }
