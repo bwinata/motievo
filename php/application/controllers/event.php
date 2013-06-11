fe48a4e9115b091d20dfebfad212a4e370a6bc18
Barry Winata <barry.winata@yahoo.com>
2013-06-11 21:32:11 +1000

    - Event creation in progress

--- php/application/controllers/event.php
+++ php/application/controllers/event.php
@@ -1,7 +1,29 @@
 <?php
 
 class Event extends CI_Controller 
-{	
+{
+	/* ============================================================== */
+	/* PRIVATE DEFINITIONS */
+	/* ============================================================== */
+	private $expected_field_names = array('event_title_name',
+										  'event_friend_name',
+										  'event_date_name',
+										  'event_time_name',
+										  'event_location_name',
+										  'event_meeting_point_name',
+										  'event_description_name');
+	
+	private $field_id = array('event_title_name' => 'event_title',
+							  'event_friend_name' => 'event_friend',
+							  'event_date_name' => 'event_date',
+							  'event_time_name' => 'event_time',
+							  'event_location_name' => 'event_location',
+							  'event_meeting_point_name' => 'event_meeting_point',
+							  'event_description_name' => 'event_description');
+							  
+	/* ============================================================== */
+	/* PUBLIC FUNCTIONS */
+	/* ============================================================== */	
 	public function organize ()
 	{
 		$data['page_title'] = 'Organize a happening | Event';
@@ -22,16 +44,32 @@ class Event extends CI_Controller
 	public function create_event ()
 	{
 		$this->load->model('Details');
-		$this->load->model('Form Processor');
+		$this->load->model('Form_Processor');
 		$this->load->model('Event_Organise', '', TRUE);
 		
 		$event_details = $this->Details->get_details();
+		$field_array = $this->combine_field_arrays($this->expected_field_names, $this->field_id);
 		
-		$this->Form_Processor->check_errors($event_details);
+		$error_check_result = $this->Form_Processor->check_errors($event_details, $field_array);
 		
-		$this->Event_Organise->create($event_details);
-		
+		if ($error_check_result['result'])
+		{
+			echo json_encode($error_check_result);
+		}
+		else
+		{
+			echo json_encode(array('response' => 'event_organised', 'result' => 'Event has been created'));
+			//echo json_encode($this->Event_Organise->create($event_details));
+		}
 	}
+	
+	/* ============================================================== */
+	/* PRIVATE FUNCTIONS */
+	/* ============================================================== */
+	private function combine_field_arrays ($field_names, $field_id)
+	{
+		return array('names' => $field_names, 'id' => $field_id);
+	}	
 }
 
 ?>
--- php/application/views/event/organise.php
+++ php/application/views/event/organise.php
@@ -50,6 +50,44 @@
 		$('.img').click(function () {
 			document.getElementById('myModal').innerHTML = "<img src=" + $(this).attr('id') + " " +  "/ >";
 		});
+		
+		$('#event_submit_btn').click(function() {
+			$.ajax({
+				method	: 'POST',
+				url		: '<?php echo site_url('event/create_event'); ?>',
+				data	: $('#event_form').serialize(),
+				dataType: 'json',
+				success	: function(data) {
+					switch(data.response)
+					{
+						case 'empty_fields':
+							document.getElementById('alert').innerHTML = 'Please make sure all fields have been filled in.';
+							document.getElementById('alert').style.display = 'block';
+	                        $.each(data.result, function(k, v) {
+	                            document.getElementById(v).style.backgroundColor = '#F9B9BF';
+	                        });						
+							break;
+						case 'valid_fields':
+							document.getElementById('alert').innerHTML = '';
+							document.getElementById('alert').style.display = 'block';
+	                        $.each(data.result, function(k, v) {
+	                            document.getElementById(v).style.backgroundColor = '#F9B9BF';
+	                        });						
+							break;
+						case 'event_organised':
+							alert(data.result);
+							break;
+						default:
+							break;
+					}
+				},
+				error	: function(data) {
+					alert('Event could not be created');
+				}
+			});
+			return false;
+		});
+		
 	});
 </script>
 
@@ -65,23 +103,28 @@
 	</div>
 	<br />
 	<div class="row">
+		<div data-alert class="alert-box alert" id="alert" style="display: none;">
+		  <a href="#" class="close">&times;</a>
+		</div>
+	</div>
+	<div class="row">
 		<div class="nav_left large-7 columns" style="margin-left: -15px;">
 			<div id="event_creation" class="container">
-				<form>
+				<form id="event_form">
 					<h5>Happening</h5>
-					<input type="text" id="event_title" class="input_fields" name="event_title_name" placeholder="What are we calling this?" />
+					<input type="text" style="border: solid;" id="event_title" class="input_fields" name="event_title_name" placeholder="What are we calling this?" />
 					<h5>Friend</h5>
-					<input type="text" id="event_friend" class="input_fields" name="event_friend_name" placeholder="Type in a name" />
+					<input type="text" style="border: solid;" id="event_friend" class="input_fields" name="event_friend_name" placeholder="Type in a name" />
 					<h5>Date</h5>
-					<input type="text" id="event_date" class="input_fields" name="event_date_time" placeholder="What date is it on?" />
+					<input type="text" style="border: solid;" id="event_date" class="input_fields" name="event_date_name" placeholder="What date is it on?" />
 					<h5>Time</h5>
-					<input type="text" id="event_time" class="input_fields" name="event_time_name" placeholder="When time is it on?" />		
+					<input type="text" style="border: solid;" id="event_time" class="input_fields" name="event_time_name" placeholder="When time is it on?" />		
 					<h5>Location</h5>
-					<input type="text" id="event_location" class="input_fields" name="event_location_name" placeholder="Where are we going?" />
+					<input type="text" style="border: solid;" id="event_location" class="input_fields" name="event_location_name" placeholder="Where are we going?" />
 					<h5>Meeting Point</h5>
-					<input type="text" id="event_meeting_point" class="input_fields" name="event_meeting_point_name" placeholder="Where are we meeting?" />				
+					<input type="text" style="border: solid;" id="event_meeting_point" class="input_fields" name="event_meeting_point_name" placeholder="Where are we meeting?" />				
 					<h5>Details</h5>
-					<textarea id="event_description" class="input_fields" name="event_description_name" style="height: 250px;" placeholder="What are we doing?"></textarea>
+					<textarea id="event_description" class="input_fields" name="event_description_name" style="height: 250px; border: solid;" placeholder="What are we doing?"></textarea>
 					<input type="submit" id="event_submit_btn" class="default button" name="event_submit_btn_name" value="Make it happen" />				
 				</form>
 			</div>				
